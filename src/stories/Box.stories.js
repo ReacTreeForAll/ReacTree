d55cc58aa51d64ad19a8ada1c9162ad3a3220d94
dasssseul <daseul8086@gmail.com>
2021-10-16 12:28:54 +0900

    add : 초기 설정 - emotion, craco, storybook, pr 템플릿

---
+++ src/stories/Box.stories.js
@@ -0,0 +1,12 @@
+import Box from "../components/Box";
+
+export default {
+    title: 'Component/Box',
+    component: Box
+}
+
+export const Default = () => {
+    return (
+        <Box />
+    )
+}
