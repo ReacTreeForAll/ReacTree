08ea454082547fec2b15e0ad3a1dfebe3e76cc30
Muntari29 <muntrock@gmail.com>
2021-10-18 01:04:58 +0900

    feat: Image 지연 로딩 기능 및 circle 스타일 추가

--- src/stories/baseComponent/Image.stories.js
+++ src/stories/baseComponent/Image.stories.js
@@ -19,9 +19,30 @@ export default {
       control: { type: 'range', min: 200, max: 600 },
     },
     mode: {
-      defaultValue: 'cover',
+      defaultValue: '',
       options: ['cover', 'fill', 'contain'],
       control: { type: 'inline-radio' },
+    },
+    block: {
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    lazy: {
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    placeholder: {
+      type: { name: 'string' },
+      defaultValue: 'https://via.placeholder.com/200',
+      control: { type: 'text' },
+    },
+    threshold: {
+      defaultValue: 0.1,
+      control: { type: 'number' },
+    },
+    iscircle: {
+      defaultValue: false,
+      control: { type: 'boolean' },
     },
   },
 }
