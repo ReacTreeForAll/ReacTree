ea877c8203f83d855f2252c1035e31fe4a4c065e
Muntari29 <muntrock@gmail.com>
2021-10-23 13:15:08 +0900

    feat: #39/useClickAway Hooks 기능 개발

--- src/components/base/Alert/index.js
+++ src/components/base/Alert/index.js
@@ -1,11 +1,11 @@
 import Swal from 'sweetalert2'
+import ImgPath from '../../../assets/pageMove.png'
 
 const Alert = () => {
   Swal.fire({
     title: '완료',
     text: '답변이 제출되었습니다',
-    imageUrl:
-      'https://cdn-icons.flaticon.com/png/512/4332/premium/4332666.png?token=exp=1634735685~hmac=552bae4d27273aac6f94f33a9bff5981',
+    imageUrl: ImgPath,
     imageHeight: 100,
     imageWidth: 100,
   })
