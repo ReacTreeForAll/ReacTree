f2315c6978f41c0c022f016b073d33707928476c
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2021-10-26 14:13:36 +0900

    feat: #9/Input - base 컴포넌트 기능 구현 (#67)
    * feat: #9/Input - base 컴포넌트 기능 개발 및 storybook 추가
    
    * refactor: #9/Input - width와 fontSize의 proptypes 수정

---
+++ src/stories/baseComponent/Input.stories.js
@@ -0,0 +1,40 @@
+import Input from '../../components/base/Input'
+
+/* eslint-disable */
+export default {
+  title: 'Components/Input',
+  component: Input,
+  argTypes: {
+    type: { control: 'text' },
+    width: { control: 'text' },
+    placeHolder: { control: 'text' },
+    fontSize: { control: 'number' },
+    border: { control: 'text' },
+  },
+}
+
+export const Default = (args) => {
+  return <Input {...args} />
+}
+
+export const SmallFont = (args) => {
+  return <Input fontSize={(args.fontSize = '16px')} {...args} />
+}
+
+export const Email = (args) => {
+  return <Input type={(args.type = 'email')} placeHolder={(args.placeHolder = 'Email')} {...args} />
+}
+
+export const Identity = (args) => {
+  return <Input type={(args.type = 'text')} placeHolder={(args.placeHolder = 'ID')} {...args} />
+}
+
+export const Password = (args) => {
+  return (
+    <Input
+      type={(args.type = 'password')}
+      placeHolder={(args.placeHolder = 'Password')}
+      {...args}
+    />
+  )
+}
