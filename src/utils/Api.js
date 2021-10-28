42e901e5a37d0a6abede728b0e9c33b21ced924e
dasssseul <daseul8086@gmail.com>
2021-10-29 01:28:55 +0900

    fix typo: url, method 위치 수정

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,8 +4,8 @@ const API_END_POINT = 'http://13.209.30.200:5003'
 
 export const RequestApi = (url, method, data) => {
   return axios({
-    method,
     url: `${API_END_POINT}${url}`,
+    method,
     data,
   })
     .then((res) => res.data)
@@ -16,8 +16,8 @@ export const Authorization = () => {
   // useSessionStorage 머지 후 변경 예정
   const tokenId = sessionStorage.getItem('tokenId')
   return axios({
-    method: 'get',
     url: `${API_END_POINT}/auth-user`,
+    method: 'get',
     headers: { Authorization: `Bearer ${tokenId}` },
   })
     .then((res) => res.data)
