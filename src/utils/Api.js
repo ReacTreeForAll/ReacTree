d12b7bdd88ed9c8c2563d8ca11b75578f19e2f74
dasssseul <daseul8086@gmail.com>
2021-10-29 23:25:52 +0900

    refactor: Api 로직 useSessionStorage 추가

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import useSessionStorage from '../hooks/useSessionStorage'
 
 const API_END_POINT = 'http://13.209.30.200:5003'
 
@@ -14,7 +15,7 @@ export const RequestApi = (url, method, data) => {
 
 export const Authorization = (url, method, data) => {
   // useSessionStorage 머지 후 변경 예정
-  const tokenId = sessionStorage.getItem('tokenId')
+  const tokenId = useSessionStorage('tokenId')
   return axios({
     url: `${API_END_POINT}${url}`,
     method,
